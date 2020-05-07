371f093174ae9d08f77c97408a68c1c973fa72d4
tayyab bhatti <tayyab.bhatti30@hotmail.com>
2020-05-07 23:26:52 +0500

    Demo - Class-level Input Validation with a Custom Attribute

--- CourseLibrary.API/Models/CourseForCreationDTO.cs
+++ CourseLibrary.API/Models/CourseForCreationDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CourseLibrary.API.ValidationAttritubes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,8 +7,11 @@ using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models
 {
+
+    [CourseTitleMustBeDifferentFromDescriptionAttributs]
+
                                         // Is applied to Impleent The Custom validation on the Entity Model
-    public class CourseForCreationDTO :IValidatableObject
+    public class CourseForCreationDTO // :IValidatableObject
     {
     
         [Required]
@@ -19,13 +23,15 @@ namespace CourseLibrary.API.Models
         public string Description { get; set; }
 
 
-        // This function is Inherit from the IvalidatableObject and use to wrie the Custom logic to show the Custom Error
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if(Title==Description)
-            {
-                yield return new ValidationResult("The Provided Descrition Should be different  from the Title", new[] {"CourseForCreationDTO"});
-            }
-        }
+        //// This function is Inherit from the IvalidatableObject and use to wrie the Custom logic to show the Custom Error
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if(Title==Description)
+        //    {
+        //        yield return new ValidationResult(
+        //            "The Provided Descrition Should be different  from the Title", 
+        //            new[] {"CourseForCreationDTO"});
+        //    }
+        //}
     }
 }
---
+++ CourseLibrary.API/ValidationAttritubes/CourseTitleMustBeDifferentFromDescriptionAttributs.cs
@@ -0,0 +1,31 @@
+﻿using CourseLibrary.API.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.ValidationAttritubes
+{
+
+    // Class Level Custom Validation Attributs.
+    public class CourseTitleMustBeDifferentFromDescriptionAttributs :ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var Course = (CourseForCreationDTO)validationContext.ObjectInstance;
+
+            if (Course.Title.ToLower().Equals(Course.Description.ToLower()))
+            {
+                return new ValidationResult(
+                    "The provided Description Must be different from title",
+                    new[] { nameof(CourseForCreationDTO) }
+                    );
+
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
